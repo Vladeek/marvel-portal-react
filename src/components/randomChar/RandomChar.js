5c89a20bb4816c6f91672d510511b670e31f028c
Vladeek <vlad.berinchikoof@gmail.com>
2022-09-18 18:49:26 +0300

    Fix some bugs

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -23,6 +23,12 @@ class RandomChar extends Component {
     this.setState({
       char,
       loading: false,
+    });
+  };
+
+  onCharLoading = () => {
+    this.setState({
+      loading: true,
       error: false,
     });
   };
@@ -36,6 +42,7 @@ class RandomChar extends Component {
 
   updateChar = () => {
     const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
+    this.onCharLoading();
     this.marvelService
       .getCharacter(id)
       .then(this.onCharLoaded)
